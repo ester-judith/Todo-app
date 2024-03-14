c7b2243edd5da5ffbc5032603138cd7f9426f40b
Ester Judith Coronado Castro <ester.j.ejcc@gmail.com>
2024-03-14 06:29:19 -0700

    Primer commit

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAw-CtvstqTHrk-M8Zb08FLEup0__OXFtE",
+  authDomain: "todo-app-87ec7.firebaseapp.com",
+  databaseURL: "https://todo-app-87ec7-default-rtdb.firebaseio.com",
+  projectId: "todo-app-87ec7",
+  storageBucket: "todo-app-87ec7.appspot.com",
+  messagingSenderId: "1031738160705",
+  appId: "1:1031738160705:web:5105e0710cc24f1a13b8c0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
